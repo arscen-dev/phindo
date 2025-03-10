c3346330f3a9949e24de4adf7f3067a7b8d551bc
arscen-dev <arscen.dev@gmail.com>
2025-03-09 22:45:11 -0400

    add test route

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,5 +1,11 @@
 import { NextResponse } from "next/server";
+import { Message } from "@/types/test";
 
-export async function GET() {
-  return NextResponse.json({ message: "Hello World" });
+export async function GET(): Promise<NextResponse<Message>> {
+  const message: Message = {
+    message: "this is a test message type",
+    status: 200,
+  };
+
+  return NextResponse.json(message);
 }
